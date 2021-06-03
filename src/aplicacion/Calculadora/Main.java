c2cb4e036eb4f6b6d3bc83a9cb894e538b61a753
AlexisHdez999 <alexishernandezmag@gmail.com>
2021-06-03 18:35:29 -0500

    cambio de version javafx

--- src/aplicacion/Calculadora/Main.java
+++ src/aplicacion/Calculadora/Main.java
@@ -2,7 +2,6 @@ package aplicacion.Calculadora;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -15,12 +14,18 @@ public class Main extends Application {
     /**
      * Metodo de inicio de la aplicaición
      * @param primaryStage
-     * @throws Exception
      */
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("InicioVista.fxml")) ;
-        VBox root = (VBox)loader.load();
+        VBox root = null;
+        try{
+            root = (VBox)loader.load();
+        } catch(Exception e){
+            System.out.println("Error al cargar la vista inicial") ;
+            return ;
+        }
+
         primaryStage.setTitle("Calculadora de sistemas de ecuaciones");
         primaryStage.setScene(new Scene(root, 600, 400));
         InicioVistaController InicioController = loader.getController() ;
